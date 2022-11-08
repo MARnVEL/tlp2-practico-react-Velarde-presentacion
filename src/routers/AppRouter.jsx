466efe97f60a4611e5f757d9d367733ebbed1045
marnvel <ricardom.velarde@gmail.com>
2022-11-08 07:03:44 -0300

    Volví a iniciar el enrutamiento 08-11-2022 07:03hs

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,22 +1,34 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { PublicRoutes } from "./PublicRoutes"
+import { PrivateRoutes } from "./PrivateRoutes"
+import { Login } from "../pages/Login"
+
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+
       <Routes>
 
-        <Route path='/login' element={ 
+        <Route path='/login' element = { 
           <PublicRoutes>
             {/* Colocar el componente a proteger */}
+            <Login />
+
           </PublicRoutes>
         } />
 
-        <Route path='/*' element={
-          // Proteger las rutas privadas
+        <Route path='/*' element = {
+          <PrivateRoutes>
+            
+            {/* Colocar el componente a proteger */}
+
+          </PrivateRoutes>
+          
         } />
 
       </Routes>
+
     </BrowserRouter>
   )
 }
