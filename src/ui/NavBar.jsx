18883830e999ec5cc884b973ccab0449ab32b95b
marnvel <ricardom.velarde@gmail.com>
2022-11-14 01:41:55 -0300

    Pequeñas modificaciones en el NavBar.jsx 14-11-2022 01:41hs

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -4,50 +4,52 @@ import { Link, NavLink } from 'react-router-dom'
 // que provee react-router-dom
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg  navbar-dark bg-dark text-light ">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          HOME
-        </NavLink>
+    <div className="conteiner">
+      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark text-light ">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            HOME
+          </NavLink>
 
-        <button className="navbar-toggler bg-danger" type="button" data-bs-toggle="collapse"                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              
-              <NavLink className="nav-link active" aria-current="page" to="/todosScreen">
-                Todo's
-              </NavLink>
+          <button className="navbar-toggler bg-danger" type="button" data-bs-toggle="collapse"                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                
+                <NavLink className="nav-link active" aria-current="page" to="/todosScreen">
+                  Todo's
+                </NavLink>
 
-            </li>
-            <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Opciones
-              </Link>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="#">Usuarios</Link></li>
-                <li><Link className="dropdown-item" to="#">Admin</Link></li>
-                <li><hr className="dropdown-divider"/></li>
-                <li>
-                  <Link className="dropdown-item" to="#">
-                    Something else here
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link disabled">Disabled</Link>
-            </li> */}
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
+              </li>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Opciones
+                </Link>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" to="#">Usuarios</Link></li>
+                  <li><Link className="dropdown-item" to="#">Admin</Link></li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li>
+                    <Link className="dropdown-item" to="#">
+                      Something else here
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              {/* <li className="nav-item">
+                <Link className="nav-link disabled">Disabled</Link>
+              </li> */}
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   )
 }
